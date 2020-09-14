cac96b311683f191ba3a7480089e8c713ea32659
Michelle Ranly <meranly8@gmail.com>
2020-09-14 02:50:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+# def batch_badge_creator(array)
+#   attendees = []
+#   array.each{|name| attendees << badge_maker(name)}
+#   attendees
+# end
+
+def batch_badge_creator(array)
+  array.collect{|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+  room_assignments = []
+  array.each_with_index{|speaker, room| room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room + 1}!"}
+  room_assignments
+end
+
+
+def printer(array)
+  batch_badge_creator(array).each{|badge| puts badge}
+  
+  assign_rooms(array).each{|room| puts room}
+end
